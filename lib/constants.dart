dfcf24d3f5edc2eb6fb919e6ac8547546114813e
Payam Jalili <payamjalili@users.noreply.github.com>
2022-07-10 07:59:14 +0200

    Build: pooshakiltay

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:bashgahapp_academy/models/enums/theme-names.dart';
+                         final ThemeNames themeName=ThemeNames.Shop; final String baseURL='https://www.pooshakiltay.ir/wp-json/v1';
+                         final String appName='پوشاک ایلتای';
+                         final String appSlogan='پوشاک اجناس ترک';
+                         final bool hasLogo=false;
---
+++ lib/themes/shop/constants.dart
@@ -0,0 +1,2 @@
+import 'package:flutter/material.dart'; const double defaultPadding = 20.0;
+                         const Color primaryColor = Color(0xFFe82525); const Color secondryColor = Color(0xFF949494);
